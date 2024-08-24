5d9ebaf9394127a449f44cd847b4f73aa4176fb0
Anastasiia Drozdovskaia <anastasia0588@gmail.com>
2024-08-24 22:43:35 +0300

    Тест страницы регистрации

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,9 +1,11 @@
 from pages.registration_page import RegistrationPage
+from pages.submit_page import SubmitWindow
 
 
 class TestRegistration:
     def test_registration_success(self, driver, user):
         registration_page = RegistrationPage(driver)
+        submit_window = SubmitWindow(driver)
 
         registration_page.wait_for_load_page()
         registration_page.fill_name_field(user['first_name'])
@@ -12,9 +14,11 @@ class TestRegistration:
         registration_page.select_any_gender()
         registration_page.fill_mobile_field(user['mobile'])
         registration_page.fill_subject_field()
+        registration_page.upload_image('testdata\\test_image.png')
         registration_page.fill_current_address_field()
-        registration_page.upload_file('testdata/test_image.png')
+        registration_page.press_submit_button()
 
-        #
+        popup_title = submit_window.get_popup_title()
+        assert popup_title == 'Thanks for submitting the form'
 
-        registration_page.select_birth_date()
+        #        registration_page.select_birth_date()
