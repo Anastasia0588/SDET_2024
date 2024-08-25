2e5bd5b25a0754a9dd9fdd7df582ff811f52505d
Anastasiia Drozdovskaia <anastasia0588@gmail.com>
2024-08-25 21:36:18 +0300

    Увеличены ожидания

--- pages/base_page.py
+++ pages/base_page.py
@@ -18,7 +18,7 @@ class BasePage:
         return self.driver.find_element(*locator)
 
     def fill_text_field(self, locator, text):
-        WebDriverWait(self.driver, 5).until((expected_conditions.element_to_be_clickable(locator)))
+        WebDriverWait(self.driver, 10).until((expected_conditions.element_to_be_clickable(locator)))
         self.driver.find_element(*locator).send_keys(text)
 
     def click_on_element(self, locator):
