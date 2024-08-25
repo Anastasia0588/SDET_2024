722f75b58edfe509d8a205df6c8817d1fcd328aa
Anastasiia Drozdovskaia <anastasia0588@gmail.com>
2024-08-25 19:03:21 +0300

    Локаторы модального окна

--- locators/submit_page_locators.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class SubmitPageLocator:
-    # локатор popup окна
-    POPUP_WINDOW = By.XPATH, './/div[@class="modal-content"]'
-    #
-    SUBMIT_POPUP_WINDOW = By.ID, 'example-modal-sizes-title-lg'
-
