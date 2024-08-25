a6d08c2929af4ed12b5a409d5543f1cf77b29e37
Anastasiia Drozdovskaia <anastasia0588@gmail.com>
2024-08-25 19:05:12 +0300

    Добавлен шаг выбора города. Проверки вынесены в отдельный метод

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,14 +1,14 @@
 import allure
 
 from pages.registration_page import RegistrationPage
-from pages.submit_page import SubmitWindow
+from pages.modal_page import ModalPage
 
 
 class TestRegistration:
     @allure.title('Заполнение формы регистрации')
     def test_registration_success(self, driver, user):
         registration_page = RegistrationPage(driver)
-        submit_window = SubmitWindow(driver)
+        modal_page = ModalPage(driver)
 
         registration_page.wait_for_load_page()
         registration_page.fill_name_field(user['first_name'])
@@ -19,10 +19,8 @@ class TestRegistration:
         registration_page.fill_subject_field()
         registration_page.upload_image('testdata\\test_image.png')
         registration_page.fill_current_address_field(user['address'])
-        registration_page.select_any_state()
+        registration_page.select_any_state(user)
+        registration_page.select_any_city(user)
         registration_page.press_submit_button()
 
-        popup_title = submit_window.get_popup_title()
-        assert popup_title == 'Thanks for submitting the form'
-
-        #        registration_page.select_birth_date()
+        modal_page.check_data_from_modal(user)
